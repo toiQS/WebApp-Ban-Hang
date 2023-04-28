ad91992acb7166942979b0091d34e9e6e6196604
toiQS <nguyensieu12112002@gmail.com>
2023-04-28 11:52:01 +0700

    allcode

---
+++ WebApp-Ban-Hang.Services/Categorys/ICategoryServices.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebApp_Ban_Hang.Services.Categorys
+{
+    public interface ICategoryServices
+    {
+    }
+}
