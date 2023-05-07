7cadd7b7653c1ba50c8ae7f0fcdf4ce3b2c97489
toiQS <nguyensieu12112002@gmail.com>
2023-05-08 04:22:25 +0700

    controller

--- WebApp-Ban-Hang/Controllers/UserDetailController.cs
+++ WebApp-Ban-Hang/Controllers/UserDetailController.cs
@@ -29,16 +29,100 @@ namespace WebApp_Ban_Hang.Controllers
             var model = new Creates();
             return View(model);
         }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(Creates model)
+        {
+            if (ModelState.IsValid)
+            {
+                var userDetail = new Creates
+                {
+                    UserName = model.UserName,
+                    UserDetailId = model.UserDetailId,
+                    CityOrProvince = model.CityOrProvince,
+                    DetaledAddress = model.DetaledAddress,
+                    District = model.District,
+                    Phone = model.Phone,
+                    WardOrVillage = model.WardOrVillage
+                };
+            }
+            return View();
+        }
         [HttpGet]
         public IActionResult Detail(int id)
         {
-            //var userDetail =  _services.FindById(id);
-            //if(userDetail == null)
-            //    return NotFound();
-            //var model = new Details
-            //{
-            //    UserDetailId = userDetail.
-            //};
+            var userDetail = _services.FindById(id);
+            if (userDetail == null)
+                return NotFound();
+            var model = new Details
+            {
+                UserDetailId = userDetail.UserDetailId,
+                UserName = userDetail.UserName,
+                CityOrProvince = userDetail.CityOrProvince,
+                DetaledAddress = userDetail.DetaledAddress,
+                District = userDetail.District,
+                Phone = userDetail.Phone,
+                WardOrVillage = userDetail.WardOrVillage,
+            };
+            return View(model);
+        }
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+            var userDetail = _services.FindById(id);
+            if(userDetail == null)
+                return NotFound();
+            var model = new Edits
+            {
+                UserName = userDetail.UserName,
+                CityOrProvince = userDetail.CityOrProvince,
+                DetaledAddress = userDetail.DetaledAddress,
+                District = userDetail.District,
+                Phone = userDetail.Phone,
+                WardOrVillage = userDetail.WardOrVillage
+            };
+            return View(model);
+        }
+        [HttpPost]
+        public async Task<IActionResult> Edit(Edits model)
+        {
+            var userDetail = _services.FindById(model.UserDetailId);
+            if (userDetail == null)
+                return NotFound();
+            userDetail.UserName = model.UserName;
+            userDetail.CityOrProvince = model.CityOrProvince;
+            userDetail.WardOrVillage = model.WardOrVillage;
+            userDetail.DetaledAddress = model.DetaledAddress;
+            userDetail.Phone = model.Phone;
+            userDetail.District = model.District;
+            return View();
+        }
+        [HttpGet]
+        public IActionResult Delete(int id)
+        {
+            var userDetail = _services.FindById(id);
+            if(userDetail == null)  
+                return NotFound();
+            var model = new Deletes
+            {
+                CityOrProvince = userDetail.CityOrProvince,
+                UserName = userDetail.UserName,
+                UserDetailId = userDetail.UserDetailId,
+                DetaledAddress = userDetail.DetaledAddress,
+                District = userDetail.Phone,
+                Phone = userDetail.District,
+                WardOrVillage = userDetail.WardOrVillage
+            };
+            return View(model);
+        }
+        [HttpPost]
+        public async Task<IActionResult> Delete(Deletes model)
+        {
+            if(ModelState.IsValid)
+            {
+                _services.DeleteById(model.UserDetailId);
+                return RedirectToAction("Index");
+            }
             return View();
         }
     }
---
+++ WebApp-Ban-Hang/Models/UserDetails/Deletes.cs
@@ -0,0 +1,13 @@
+﻿namespace WebApp_Ban_Hang.Models.UserDetails
+{
+    public class Deletes
+    {
+        public int UserDetailId { get; set; }
+        public string UserName { get; set; }
+        public string? DetaledAddress { get; set; }
+        public string WardOrVillage { get; set; }
+        public string District { get; set; }
+        public string CityOrProvince { get; set; }
+        public string Phone { get; set; }
+    }
+}
