e13f192ded44c64887e1437e8d6debd998368782
toiQS <nguyensieu12112002@gmail.com>
2023-05-11 19:50:12 +0700

    repair

--- WebApp-Ban-Hang/Program.cs
+++ WebApp-Ban-Hang/Program.cs
@@ -57,7 +57,7 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Product}/{action=Index}/{id?}");
+    pattern: "{controller=Home}/{action=Index}/{id?}");
 app.MapRazorPages();
 
 app.Run();
